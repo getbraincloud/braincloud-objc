5eb64632c4a6c6e643d7b8a99f5778d76dddcd31
David St-Louis <davidstl@bitheads.com>
2021-12-07 07:54:39 -0500

    BCLOUD-1332 Better doc around readFriendUserState

--- Shared/BrainCloudFriend.hh
+++ Shared/BrainCloudFriend.hh
@@ -122,6 +122,8 @@
 
 /**
 * Read a friend's user state.
+* If you are not friend with this user, you will get an error
+* with NOT_FRIENDS reason code.
 *
 * Service Name - PlayerState
 * Service Operation - ReadFriendsPlayerState
