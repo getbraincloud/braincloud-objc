06bf3dd4b13421b807d57ebd66d1e0b149c91755
David St-Louis <davidstl@bitheads.com>
2021-12-07 07:45:49 -0500

    BCLOUD-1314 fixed wrong doc for getSocialLB

--- Shared/BrainCloudLeaderboard.hh
+++ Shared/BrainCloudLeaderboard.hh
@@ -29,10 +29,11 @@ typedef NS_ENUM(NSUInteger, SortOrder) { HIGH_TO_LOW, LOW_TO_HIGH };
 /**
 * Method returns the social leaderboard. A player's social leaderboard is
 * comprised of players who are recognized as being your friend.
-* For now, this applies solely to Facebook connected players who are
-* friends with the logged in player (who also must be Facebook connected).
-* In the future this will expand to other identification means (such as
-* Game Centre, Google circles etc).
+* 
+* The getSocialLeaderboard will retrieve all friends from all friend platforms, so
+* - all external friends (Facebook, Steam, PlaystationNetwork)
+* - all internal friends (brainCloud)
+* - plus "self".
 *
 * Leaderboards entries contain the player's score and optionally, some user-defined
 * data associated with the score. The currently logged in player will also
@@ -58,10 +59,11 @@ typedef NS_ENUM(NSUInteger, SortOrder) { HIGH_TO_LOW, LOW_TO_HIGH };
 /**
  * Method returns the social leaderboard by its version. A player's social leaderboard is
  * comprised of players who are recognized as being your friend.
- * For now, this applies solely to Facebook connected players who are
- * friends with the logged in player (who also must be Facebook connected).
- * In the future this will expand to other identification means (such as
- * Game Centre, Google circles etc).
+ * 
+ * The getSocialLeaderboard will retrieve all friends from all friend platforms, so
+ * - all external friends (Facebook, Steam, PlaystationNetwork)
+ * - all internal friends (brainCloud)
+ * - plus "self".
  *
  * Leaderboards entries contain the player's score and optionally, some user-defined
  * data associated with the score. The currently logged in player will also
