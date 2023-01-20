2be2824508e88862a682066fc1ddf778689feada
jo-codegirl <joanneh@bitheads.com>
2023-01-20 13:25:20 -0700

    doing a subset of tests from commandline

--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-xcodebuild test -workspace BrainCloud.xcworkspace -destination "platform=iOS Simulator,name=iPhone 8 Plus" -scheme BrainCloud-iOS
+
+for var in "$@"
+do
+	COMMAND="$COMMAND -only-testing:BrainCloud-iOSTests/Test$var"
+done
+
+xcodebuild test -workspace BrainCloud.xcworkspace -destination "platform=iOS Simulator,name=iPhone 8 Plus" -scheme BrainCloud-iOS  ${COMMAND}
