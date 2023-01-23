3a808cf2725d7a50617c5cb1fcd4744360cecec8
jo-codegirl <joanneh@bitheads.com>
2023-01-23 10:48:10 -0700

    Put back to running all tests

--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -5,4 +5,4 @@ do
 	COMMAND="$COMMAND -only-testing:BrainCloud-iOSTests/Test$var"
 done
 
-xcodebuild test -workspace BrainCloud.xcworkspace -destination "platform=iOS Simulator,name=iPhone 8 Plus" -scheme BrainCloud-iOSRunSet  ${COMMAND}
+xcodebuild test -workspace BrainCloud.xcworkspace -destination "platform=iOS Simulator,name=iPhone 8 Plus" -scheme BrainCloud-iOS  ${COMMAND}
