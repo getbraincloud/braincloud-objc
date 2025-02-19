90a9aa6a164a77db3c0fe18bc5b5c289a4a497db
franco <82105876+francobithead@users.noreply.github.com>
2025-02-19 15:19:56 -0500

    Forgot a spot for BCLOUD-9289

--- Shared/BrainCloudClient.hh
+++ Shared/BrainCloudClient.hh
@@ -169,7 +169,7 @@ typedef NS_ENUM(NSUInteger, BCBrainCloudUpdateType)
 
 /**
  * Method initializes the BrainCloudClient with a map of appid->secretkey
- * Automatically uses current serverURL https://sharedprod.braincloudservers.com/dispatcherv2
+ * Automatically uses current serverURL https://api.braincloudservers.com/dispatcherv2
  *
  * @param defaultAppId The default app id to start with
  * @param secretMap All app ids to secret keys used by this application
