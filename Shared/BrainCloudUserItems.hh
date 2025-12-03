faeebc5d1f5dce70517cdcb5b9d78f88a3364444
franco <82105876+francobithead@users.noreply.github.com>
2025-12-03 11:52:46 -0500

    Declared functions + documentation was easier in VS code

--- Shared/BrainCloudUserItems.hh
+++ Shared/BrainCloudUserItems.hh
@@ -241,6 +241,107 @@ errorCompletionBlock:(BCErrorCompletionBlock)ecb
 errorCompletionBlock:(BCErrorCompletionBlock)ecb
            cbObject:(BCCallbackObject)cbObject;
 
+/**
+ * Allows item(s) to be awarded to a user without 
+ * collecting the purchase amount. If includeDef 
+ * is true, response includes associated itemDef 
+ * with language fields limited to the current 
+ * or default language.
+ * 
+ * Service Name - userItems
+ * Service Operation - AWARD_USER_ITEM
+ * 
+ * @param defId - The unique id of the item definition to award.
+ * @param quantity - The quantity of the item to award.
+ * @param includeDef - If true, the associated item definition will be included in the response.
+ * @param optionsJson -  Optional support for specifying 'blockIfExceedItemMaxStackable' indicating
+ *  how to process the award if the defId is for a stackable item with a max stackable quantity and 
+ * the specified quantity to award is too high. If true and the quantity is too high, the call is 
+ * blocked and an error is returned. If false (default) and quantity is too high, the quantity is 
+ * adjusted to the allowed maximum and the quantity not awarded is reported in response key 
+ * 'itemsNotAwarded' - unless the adjusted quantity would be 0, in which case the call is blocked 
+ * and an error is returned.
+ */
+- (void)awardUserItemWithOptions:(NSString *)defId
+            quantity:(int)quantity
+         includeDef:(bool)includeDef
+         optionsJson:(NSString *)optionsJson
+    completionBlock:(BCCompletionBlock)completionBlock
+errorCompletionBlock:(BCErrorCompletionBlock)ecb
+           cbObject:(BCCallbackObject)cbObject;
+
+
+
+/**
+ * Purchases a quantity of an item from the specified store, if the user has enough funds and 
+ * purchasing for listed buy price is not disabled for associated catalog item definition. 
+ * If includeDef is true, response includes associated itemDef with language fields limited to 
+ * the current or default language.
+ * 
+ * Service Name - userItems
+ * Service Operation - PURCHASE_USER_ITEM
+ * 
+ * @param defId - The unique id of the item definition to award.
+ * @param quantity - The quantity of the item to award.
+ * @param shopId - The id identifying the store the item is being purchased from, if applicable.
+ * @param includeDef - If true, the associated item definition will be included in the response.
+ * @param optionsJson -  Optional support for specifying 'blockIfExceedItemMaxStackable' indicating
+ *  how to process the award if the defId is for a stackable item with a max stackable quantity and 
+ * the specified quantity to award is too high. If true and the quantity is too high, the call is 
+ * blocked and an error is returned. If false (default) and quantity is too high, the quantity is 
+ * adjusted to the allowed maximum and the quantity not awarded is reported in response key 
+ * 'itemsNotAwarded' - unless the adjusted quantity would be 0, in which case the call is blocked 
+ * and an error is returned.
+ */
+- (void)purchaseUserItemWithOptions:(NSString *)defId
+            quantity:(int)quantity
+            shopId:(NSString *)shopId
+         includeDef:(bool)includeDef
+         optionsJson:(NSString *)optionsJson
+    completionBlock:(BCCompletionBlock)completionBlock
+errorCompletionBlock:(BCErrorCompletionBlock)ecb
+           cbObject:(BCCallbackObject)cbObject;
+
+
+/**
+ * Returns list of items on promotion available to the current user.
+ * 
+ * Service Name - userItems
+ * Service Operation - GET_ITEMS_ON_PROMOTION
+ * 
+ * @param shopId - The id identifying the store the item is being purchased from, if applicable.
+ * @param includeDef - If true, the associated item definition will be included in the response.
+ * @param includePromotionDetails - If true, the promotion details of the eligible promotions will be included in the response.
+ */
+- (void)getItemsOnPromotion:(NSString *)shopId
+         includeDef:(bool)includeDef
+         includePromotionDetails:(bool)includePromotionDetails
+    completionBlock:(BCCompletionBlock)completionBlock
+errorCompletionBlock:(BCErrorCompletionBlock)ecb
+           cbObject:(BCCallbackObject)cbObject;
+
+
+/**
+ * Returns list of items on promotion available to the current user.
+ * 
+ * Service Name - userItems
+ * Service Operation - GET_ITEMS_ON_PROMOTION
+ * 
+ * @param defId - The unique id of the item definition to check.
+ * @param shopId - The id identifying the store the item is being purchased from, if applicable.
+ * @param includeDef - If true, the associated item definition will be included in the response.
+ * @param includePromotionDetails - If true, the promotion details of the eligible promotions will be included in the response.
+ */
+- (void)getItemPromotionDetails:(NSString *)defId
+          shopId:(NSString *)shopId
+         includeDef:(bool)includeDef
+         includePromotionDetails:(bool)includePromotionDetails
+    completionBlock:(BCCompletionBlock)completionBlock
+errorCompletionBlock:(BCErrorCompletionBlock)ecb
+           cbObject:(BCCallbackObject)cbObject;
+
+
+
 //RTT BLOCKCHAIN - will be available when RTT confirmed
 
 /**
