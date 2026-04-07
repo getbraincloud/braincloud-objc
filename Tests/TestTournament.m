7fa025b297f2384af73e1876b0629f3d54ab229a
bitheadCody <codym@bitheads.com>
2026-04-07 14:03:15 -0400

    implemented group tournament APIs

--- Tests/TestTournament.m
+++ Tests/TestTournament.m
@@ -18,6 +18,8 @@ NSString *_tournamentCode = @"testTournament";
 NSString *_leaderboardId = @"testTournamentLeaderboard";
 NSString *_divSetId = @"testDivSetId";
 NSString *_invalidId = @"Invalid_Id";
+NSString *_groupLeaderboardId = @"groupTournament";
+NSString *_groupId = nil;
 
 BOOL _didJoin = false;
 
@@ -158,6 +160,125 @@ BOOL _didJoin = false;
     [self waitForFailedResult];
 }
 
+- (void)testGetGroupDivisionInfo
+{
+    [self createGroupForTournament];
+
+    [[m_client tournamentService] getGroupDivisionInfo:@"bronzeGroup"
+                                               groupId:_groupId
+                                       completionBlock:successBlock
+                                  errorCompletionBlock:failureBlock
+                                              cbObject:nil];
+    [self waitForResult];
+
+    [self deleteGroupForTournament];
+}
+
+- (void)testGetGroupDivisions
+{
+    [self createGroupForTournament];
+
+    [[m_client tournamentService] getGroupDivisions:_groupId
+                                    completionBlock:successBlock
+                               errorCompletionBlock:failureBlock
+                                           cbObject:nil];
+    [self waitForResult];
+
+    [self deleteGroupForTournament];
+}
+
+- (void)testGetGroupTournamentStatus
+{
+    [self createGroupForTournament];
+
+    [[m_client tournamentService] getGroupTournamentStatus:_groupLeaderboardId
+                                                   groupId:_groupId
+                                                 versionId:-1
+                                           completionBlock:successBlock
+                                      errorCompletionBlock:failureBlock
+                                                  cbObject:nil];
+    [self waitForResult];
+
+    [self deleteGroupForTournament];
+}
+
+- (void)testJoinAndLeaveGroupDivision
+{
+    [self createGroupForTournament];
+
+    [[m_client tournamentService] joinGroupDivision:@"bronzeGroup"
+                                     tournamentCode:@"testGroupTournament"
+                                            groupId:_groupId
+                                       initialScore:0
+                                    completionBlock:successBlock
+                               errorCompletionBlock:failureBlock
+                                           cbObject:nil];
+    [self waitForResult];
+
+    NSDictionary *data = [TestFixtureBase getDataFromResponse:self.jsonResponse];
+    NSString *leaderboardId = [data objectForKey:@"leaderboardId"];
+    XCTAssertNotNil(leaderboardId, @"Error reading joinGroupDivision response leaderboardId");
+
+    [[m_client tournamentService] leaveGroupDivisionInstance:leaderboardId
+                                                     groupId:_groupId
+                                             completionBlock:successBlock
+                                        errorCompletionBlock:failureBlock
+                                                    cbObject:nil];
+    [self waitForResult];
+
+    [self deleteGroupForTournament];
+}
+
+- (void)testJoinPostLeaveGroupTournament
+{
+    [self createGroupForTournament];
+
+    NSDate *now = [NSDate date];
+    NSTimeInterval nowEpochMillis = [now timeIntervalSince1970] * 1000;
+
+    [[m_client tournamentService] joinGroupTournament:_groupLeaderboardId
+                                       tournamentCode:@"testGroupTournament"
+                                              groupId:_groupId
+                                         initialScore:0
+                                      completionBlock:successBlock
+                                 errorCompletionBlock:failureBlock
+                                             cbObject:nil];
+    [self waitForResult];
+
+    [[m_client tournamentService] postGroupTournamentScore:_groupLeaderboardId
+                                                   groupId:_groupId
+                                                     score:10
+                                                  jsonData:nil
+                                       roundStartedTimeUTC:nowEpochMillis
+                                           completionBlock:successBlock
+                                      errorCompletionBlock:failureBlock
+                                                  cbObject:nil];
+    [self waitForResult];
+
+    [[m_client tournamentService] postGroupTournamentScoreWithResults:_groupLeaderboardId
+                                                              groupId:_groupId
+                                                                score:100
+                                                             jsonData:nil
+                                                  roundStartedTimeUTC:nowEpochMillis
+                                                           sortOrder:HIGH_TO_LOW
+                                                         beforeCount:10
+                                                          afterCount:10
+                                                        initialScore:0
+                                                     completionBlock:successBlock
+                                                errorCompletionBlock:failureBlock
+                                                            cbObject:nil];
+    [self waitForResult];
+
+    [[m_client tournamentService] leaveGroupTournament:_groupLeaderboardId
+                                               groupId:_groupId
+                                       completionBlock:successBlock
+                                  errorCompletionBlock:failureBlock
+                                              cbObject:nil];
+    [self waitForResult];
+
+    [self deleteGroupForTournament];
+}
+
 - (int)joinTournament
 {
     [[m_client tournamentService] joinTournament:_leaderboardId
@@ -184,6 +305,37 @@ BOOL _didJoin = false;
                                          cbObject:nil];
     [self waitForResult];
     _didJoin = false;
+}
+
+- (void)createGroupForTournament
+{
+    [[m_client groupService] createGroup:@"testGroup"
+                               groupType:@"csharpTest"
+                             isOpenGroup:YES
+                                     acl:@"{ \"other\": 2, \"member\": 2 }"
+                                jsonData:@"{}"
+                     jsonOwnerAttributes:@"{}"
+             jsonDefaultMemberAttributes:@"{}"
+                         completionBlock:successBlock
+                    errorCompletionBlock:failureBlock
+                                cbObject:nil];
+    [self waitForResult];
+
+    NSDictionary *data = [TestFixtureBase getDataFromResponse:self.jsonResponse];
+    _groupId = [data objectForKey:@"groupId"];
+}
+
+- (void)deleteGroupForTournament
+{
+    if (!_groupId) return;
+
+    [[m_client groupService] deleteGroup:_groupId
+                                 version:-1
+                         completionBlock:successBlock
+                    errorCompletionBlock:failureBlock
+                                cbObject:nil];
+    [self waitForResult];
+    _groupId = nil;
 }
 
 @end
